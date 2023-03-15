b2bcdd604ef8b77fa71d9838531901c575bf3a98
정승현 <tmdgusya@naver.com>
2023-03-15 22:33:16 +0900

    init

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -7,8 +7,10 @@
 
 import SwiftUI
 
+// @main annotated that this is entry point
 @main
 struct LandmarksApp: App {
+    // body must provide scenes which in turn provide content for display (one or more)
     var body: some Scene {
         WindowGroup {
             ContentView()
