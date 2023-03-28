c3d299a1a94261ad7fd74bbba6b6b4b6f2ed8785
정승현 <tmdgusya@naver.com>
2023-03-28 18:07:18 +0900

    add poor guy app

---
+++ I Am Poor/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by roach on 2023/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
